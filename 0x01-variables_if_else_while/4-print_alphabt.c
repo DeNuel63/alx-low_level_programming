ce230e036a7f69fea457ee3a3ea95524d5ae1659
DeNuel63 <oluwadamisi6363@gmail.com>
2022-09-10 13:36:04 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alphabt;
+	alphabt = 'a';
+	while (alphabt <= 'z')
+	{
+		if (alphabt == 'e')
+	}
+	else
+	{
+		if (alphabt == 'q')
+	}
+	else
+	{
+		putchar(alphabt);
+		alphabt++;
+	}
+	putchar('\n');
+	return (0);
+}
