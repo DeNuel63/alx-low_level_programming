d6c0bf0e46e267bb040c0379df36836b3197cb98
DeNuel63 <oluwadamisi6363@gmail.com>
2022-09-14 19:59:27 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_alphabet - function to print alphabet
+ *
+ * Return: returns void on sucess
+ */
+void print_alphabet(void)
+{
+	int alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(alph)
+		alph++;
+	}
+	putchar('\n');
+}
+
